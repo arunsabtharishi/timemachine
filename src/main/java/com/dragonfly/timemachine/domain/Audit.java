c36deb1212dccb3262914e5dde171baef9983de6
arunsabtharishi <arun.sabtharishi@gmail.com>
2016-04-13 23:17:56 -0500

    Fixing compilation error

--- src/main/java/com/dragonfly/timemachine/domain/Audit.java
+++ src/main/java/com/dragonfly/timemachine/domain/Audit.java
@@ -1,7 +1,5 @@
 package com.dragonfly.timemachine.domain;
 
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-public class Audit extends AuditingEntityListener {
+public class Audit {
 
 }
